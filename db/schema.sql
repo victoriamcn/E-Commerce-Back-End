91ec324e74d58576a90efbeae680f0665ebc08f4
Victoria <vmcnorrill@gmail.com>
2023-04-21 21:45:08 -0400

    debugged foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -4,11 +4,11 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
-USE ecommerce_db
+USE ecommerce_db;
 
 CREATE TABLE Category (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    category_name VARCHAR(30) NOT NULL
+    category_name VARCHAR(30) NOT NULL,
 );
 
 CREATE TABLE Product (
@@ -16,16 +16,26 @@ CREATE TABLE Product (
     product_name VARCHAR(30) NOT NULL,
     price DECIMAL NOT NULL, -- Validates that the value is a decimal
     stock INT(10) NOT NULL, -- Validates that the value is numeric
-    category_id INT FOREIGN KEY (category_id) REFERENCES Category(id)
+    category_id INT,
+    FOREIGN KEY (category_id) 
+    REFERENCES Category(id)
+    ON DELETE CASCADE,
 );
+
 
 CREATE TABLE Tag (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    tag_name VARCHAR(30)
+    tag_name VARCHAR(30),
 );
 
 CREATE TABLE ProductTag (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    product_id INT FOREIGN KEY (product_id) REFERENCES Product(id),
-    tag_id INT FOREIGN KEY (tag_id) REFERENCES Tag(id)
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	product_id INT,
+	FOREIGN KEY (product_id) 
+	REFERENCES Product (id)
+	ON DELETE CASCADE,
+	tag_id INT, 
+	FOREIGN KEY (tag_id)
+	REFERENCES Tag (id)
+	ON DELETE CASCADE,
 );
