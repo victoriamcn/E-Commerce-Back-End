6eeefab1ab46203285053742189688622f20bae0
Victoria <vmcnorrill@gmail.com>
2023-04-22 15:14:55 -0400

    writing category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,31 +1,54 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
+const seedProducts = require('../../seeds/product-seeds');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  // GET all categories: be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll(
+      // JOIN with products, using the through table
+    );
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
   console.log("Category GET Request")
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  // GET one category by its `id` value: be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params, id, {
+      // JOIN with products, using the through table
+      include [{ model: Product, through: Category }]
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   console.log("Category id GET Request")
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
   console.log("Category POST Request")
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   console.log("Category id POST Request")
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   console.log("Category id DELETE Request")
 });
