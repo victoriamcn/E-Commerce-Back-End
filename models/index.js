343d2e7fdc3d104d85aae6eaeb31598006856fd5
Victoria <vmcnorrill@gmail.com>
2023-04-22 14:51:34 -0400

    wrote models for Pk and Fk connections

--- models/index.js
+++ models/index.js
@@ -3,15 +3,42 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const seedProducts = require('../seeds/product-seeds');
+const seedTags = require('../seeds/tag-seeds');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'id',
+  onDelete: 'CASCADE'
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+//third table needed to store the foreign keys
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }//,
+  // Define an alias for when data is retrieved
+  //as: 'product_tags'
+})
 
 // Tags belongToMany Products (through ProductTag)
+//third table needed to store the foreign keys
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  }
+})
 
+
+//export is deconstructed
 module.exports = {
   Product,
   Category,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,24 +6,28 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  console.log("categories here")
+  console.log("Category GET Request")
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  console.log("Category id GET Request")
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  console.log("Category POST Request")
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  console.log("Category id POST Request")
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  console.log("Category id DELETE Request")
 });
 
 module.exports = router;
