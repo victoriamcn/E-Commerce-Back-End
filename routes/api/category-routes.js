a7f76925a485ad87ecae71abfd73375a4cc9f6a8
Victoria <vmcnorrill@gmail.com>
2023-04-22 15:35:29 -0400

    categories route: POST, PUT, DELETE completed

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -46,16 +46,48 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
   console.log("Category POST Request")
 });
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   console.log("Category id POST Request")
 });
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No categories found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   console.log("Category id DELETE Request")
 });
 
